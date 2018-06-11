b8302e9648945beb2b086ff9831513c363e3c299
ignitz <ignitzhjfk@gmail.com>
2018-06-11 19:11:54 -0300

    do_test

---
+++ tp4/assignments/PA4/do_test.sh
@@ -0,0 +1,46 @@
+pause() {
+  read -r -p "Press ENTER/RETURN key to continue..." key
+}
+
+./mysemant test/arith.cl
+pause
+./mysemant test/atoi.cl
+pause
+./mysemant test/atoi_test.cl
+pause
+./mysemant test/bad.cl
+pause
+./mysemant test/book_list.cl
+pause
+./mysemant test/cells.cl
+pause
+./mysemant test/complex.cl
+pause
+./mysemant test/cool.cl
+pause
+./mysemant test/g1.graph
+pause
+./mysemant test/good.cl
+pause
+./mysemant test/graph.cl
+pause
+./mysemant test/hairyscary.cl
+pause
+./mysemant test/hello_world.cl
+pause
+./mysemant test/io.cl
+pause
+./mysemant test/lam.cl
+pause
+./mysemant test/life.cl
+pause
+./mysemant test/list.cl
+pause
+./mysemant test/new_complex.cl
+pause
+./mysemant test/palindrome.cl
+pause
+./mysemant test/primes.cl
+pause
+./mysemant test/sort_list.cl
+pause
