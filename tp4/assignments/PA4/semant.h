323338eef3dd5d6c0bbfa2e2bb6105b10a74e19b
ignitz <ignitzhjfk@gmail.com>
2018-06-11 15:15:26 -0300

    Prettier in 3 files before edit

--- tp4/assignments/PA4/semant.h
+++ tp4/assignments/PA4/semant.h
@@ -2,7 +2,7 @@
 #define SEMANT_H_
 
 #include <assert.h>
-#include <iostream>  
+#include <iostream>
 #include "cool-tree.h"
 #include "stringtab.h"
 #include "symtab.h"
@@ -19,20 +19,19 @@ typedef ClassTable *ClassTableP;
 // you like: it is only here to provide a container for the supplied
 // methods.
 
-class ClassTable {
+class ClassTable
+{
 private:
   int semant_errors;
   void install_basic_classes();
-  ostream& error_stream;
+  ostream &error_stream;
 
 public:
   ClassTable(Classes);
   int errors() { return semant_errors; }
-  ostream& semant_error();
-  ostream& semant_error(Class_ c);
-  ostream& semant_error(Symbol filename, tree_node *t);
+  ostream &semant_error();
+  ostream &semant_error(Class_ c);
+  ostream &semant_error(Symbol filename, tree_node *t);
 };
 
-
 #endif
-
