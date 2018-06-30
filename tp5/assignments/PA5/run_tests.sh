25e5d04c603d3d17eceda50179e56c19ec7aaae3
ignitz <ignitzhjfk@gmail.com>
2018-06-29 21:57:36 -0300

    run tests

--- tp5/assignments/PA5/run_tests.sh
+++ tp5/assignments/PA5/run_tests.sh
@@ -7,6 +7,11 @@ exec_spim() {
   ../../bin/spim $1 > $1.log
 }
 
+check_diff() {
+  temp=$(echo $1 | cut -d'/' -f 2)
+  diff $1.log tests/output/${temp}.log
+}
+
 exec_spim tests/book_list.s
 exec_spim tests/cells.s
 exec_spim tests/complex.s
@@ -19,3 +24,16 @@ exec_spim tests/lam.s
 exec_spim tests/list.s
 exec_spim tests/new_complex.s
 exec_spim tests/primes.s
+
+check_diff tests/cool.s
+check_diff tests/cells.s
+check_diff tests/complex.s
+check_diff tests/cool.s
+check_diff tests/graph.s
+check_diff tests/hairyscary.s
+check_diff tests/hello_world.s
+check_diff tests/io.s
+check_diff tests/lam.s
+check_diff tests/list.s
+check_diff tests/new_complex.s
+check_diff tests/primes.s
